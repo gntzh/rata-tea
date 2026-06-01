a6a116e1f030d0dfa6356b22e4bbf21a2da11358
Grant Zhang <grant.zhang.s@outlook.com>
2026-06-01 15:56:42 +0800

    style: format code

--- examples/readme.rs
+++ examples/readme.rs
@@ -1,6 +1,6 @@
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
-use ratatui::{Frame, widgets::Paragraph};
 use rata_tea::{Action, Application, Cmd, Runner, Sub, terminal::on_key_press};
+use ratatui::{Frame, widgets::Paragraph};
 
 fn main() -> color_eyre::Result<()> {
     let app = Application::new(Model::new, Model::update, Model::view)
